22389bf89f80187d82610e7c642d10f8a2c47331
Jake Hyer <jakehyer@Jakes-MacBook-Air.local>
2019-04-16 15:42:52 -0600

    finised all except black diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
   }
-
+componentDidMount(){
+Axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+  this.setState({
+    students: res.data
+  })
+})
+}
   render() {
+    let mappedStudents = this.state.students.map((current, index) => (
+      <Link key={index} to={`/student/${current.id}`}>
+        <h3>{current.first_name} {current.last_name}</h3>
+      </Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
